7a3757ef346b5e05a787b34530e2c6f40e81ec9c
gx <fairwinds0099@gmail.com>
2019-11-14 19:48:32 -0600

    secondary commit

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -1,3 +1,4 @@
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import model.Schema;
 import org.junit.Test;
@@ -10,12 +11,18 @@ public class Parser extends Base {
     public void parseMe() throws IOException {
 
         ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+
         Schema schema = objectMapper.readValue(schemaJson, Schema.class);
         System.out.println(
 
 
                 schema.getDefaults().getFigures().getFigureKeyFive() + "\n" +
-                        schema.getParametric().getFigures().getFigureKeyFive()
+                        schema.getParametric().getFigures().getFigureKeyFive() + "\n"
+                        +
+                        schema.getDefaults().getFigures().getFigureKeyOne() + "\n" +
+                        schema.getParametric().getFigures().getFigureKeyOne()
+
         );
 
     }
